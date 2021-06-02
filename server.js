c3a94266bb72d8f56be897904530e7386d57075c
shimon tadesa <shimontadesa123@gmail.com>
2021-06-02 14:38:32 +0300

    a

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
