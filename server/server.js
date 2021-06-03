aa866416f6b62a918f26d49ecc0182d8ea1b812c
shimon tadesa <shimontadesa123@gmail.com>
2021-06-03 16:13:52 +0300

    add script

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
+const path = require('path');
 
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
